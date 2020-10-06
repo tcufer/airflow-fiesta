d9ca085cf8e73394eeedb87eca035eeba263ae11
tcufer <zamot101@gmail.com>
2020-10-06 23:39:39 +0200

    Simple sensors.

--- plugins/demo_plugin.py
+++ plugins/demo_plugin.py
@@ -1,7 +1,10 @@
 from airflow.plugins_manager import AirflowPlugin
 from airflow.models import BaseOperator
+from airflow.operators.sensors import BaseSensorOperator
 import logging as log
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.fs_hook import FSHook
+import os, stat
 
 class DataTransferOperator(BaseOperator):
 
@@ -28,19 +31,39 @@ class DataTransferOperator(BaseOperator):
       fout = open(DestinationFile, "a")
 
       for line in fin:
-        log.info("### reading line: %s", line)
-        for word in DeleteList:
-          log.info("### matching string: %s", word)
-          line = line.replace(word, "")
+          log.info("### reading line: %s", line)
+          for word in DeleteList:
+              log.info("### matching string: %s", word)
+              line = line.replace(word, "")
 
-        log.info("### output line is: %s", line)
-        fout.write(line)
+          log.info("### output line is: %s", line)
+          fout.write(line)
 
       fin.close()
       fout.close()
 
+class FileCountSensor(BaseSensorOperator):
+
+    @apply_defaults
+    def __init__(self, dir_path, conn_id, *args, **kwargs):
+      self.dir_path = dir_path
+      self.conn_id = conn_id
+      super().__init__(*args, **kwargs)
+
+    def poke(self, context):
+        hook = FSHook(self.conn_id)
+        basepath = hook.get_path()
+        full_path = os.path.join(basepath, self.dir_path)
+        self.log.info('poking location %s', full_path)
+        try:
+            for root, dirs, files in os.walk(full_path):
+                if len(files) >= 5:
+                    return True
+        except OSError:
+            return False
+        return False
 
 class DemoPlugin(AirflowPlugin):
     name = "demo_plugin"
     operators = [DataTransferOperator]
-    sensors = []
+    sensors = [FileCountSensor]
