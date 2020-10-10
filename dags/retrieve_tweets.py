c99558d9484fb79f743316d2c76b97a84e83cf75
tcufer <zamot101@gmail.com>
2020-10-10 23:51:59 +0200

    Read tweets from a user.

---
+++ dags/retrieve_tweets.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+from all_tweets import get_all_tweets
+
+yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 10, 10),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
+
+  t1 = PythonOperator(task_id='clean_raw_csv', python_callable=get_all_tweets)
+
+t1
