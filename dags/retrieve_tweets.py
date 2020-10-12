4ba86df7ca96e1a6ae4cb8525c5f09602b9aef29
tcufer <zamot101@gmail.com>
2020-10-12 23:17:50 +0200

    Minor fix.

--- dags/retrieve_tweets.py
+++ dags/retrieve_tweets.py
@@ -9,13 +9,13 @@ yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2020, 10, 10),
+    "start_date": datetime(2020, 10, 11),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
 
 with DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
 
-  t1 = PythonOperator(task_id='clean_raw_csv', python_callable=get_all_tweets)
+  t1 = PythonOperator(task_id='twitter_feed', python_callable=get_all_tweets)
 
 t1
