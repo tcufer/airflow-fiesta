e4432fb9edf4ee7c359150df74c6dc7543405df2
tcufer <zamot101@gmail.com>
2020-11-01 22:34:29 +0100

    Typo.

--- sql_files/create_twitter_data.sql
+++ sql_files/create_twitter_data.sql
@@ -1,1 +1,1 @@
-CREATE TABLE IF NOT EXISTS twitter_data(id varchar(20), created datetime, content text, favorite_cnt int, retweet_count int);
+CREATE TABLE IF NOT EXISTS twitter_data(id varchar(20), created timestamp, content text, favorite_cnt int, retweet_count int);
