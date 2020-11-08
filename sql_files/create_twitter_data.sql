c680c76613dc6982e433ac4a86b9ef5d201866be
tcufer <zamot101@gmail.com>
2020-11-08 23:56:51 +0100

    Fix create table script.

--- sql_files/create_twitter_data.sql
+++ sql_files/create_twitter_data.sql
@@ -1,1 +1,3 @@
-CREATE TABLE IF NOT EXISTS twitter_data(id varchar(20), created timestamp, content text, favorite_cnt int, retweet_count int);
+CREATE TABLE IF NOT EXISTS twitter_data(id varchar(20), created timestamp, content text, favorite_count int, retweet_count int);
+
+
