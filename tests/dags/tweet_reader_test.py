5231bd4f23369d5ae0c76ba04a00df444ce985db
tcufer <zamot101@gmail.com>
2020-11-08 23:56:30 +0100

    Testing.

---
+++ tests/dags/tweet_reader_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import unittest
+from mock import Mock, patch
+
+from dags import tweet_reader
+
+class TweetReaderTest(unittest.TestCase)
+
+    def get_all_tweets(self)
+        mock_get_patcher = patch('dags.requests.get')
+
+if __name__ == "__main__":
+    unittest.main()
