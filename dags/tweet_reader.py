9ebac007f313f6a6b6aebea39a0d7bcf209a529a
tcufer <zamot101@gmail.com>
2020-11-11 22:00:47 +0100

    Read tweets from multiple profiles.

--- dags/tweet_reader.py
+++ dags/tweet_reader.py
@@ -16,8 +16,8 @@ class TweetReader():
     config = ""
     with open('./dags/secrets.yml', 'r') as file:
       config = yaml.safe_load(file)
-    #pass in the username of the account you want to download
-    screen_name = config['user_profile']
+    #pass in the username of the account(s) you want to download
+    screen_names = config['user_profiles']
     # read credentials
     consumer_key = config['consumer_key']
     consumer_secret = config['consumer_secret']
@@ -33,47 +33,49 @@ class TweetReader():
     alltweets = []
     noRT = []
 
-    #make initial request for most recent tweets with extended mode enabled to get full tweets
-    new_tweets = api.user_timeline(screen_name = screen_name, tweet_mode = 'extended', count=200)
-
-    #save most recent tweets
-    alltweets.extend(new_tweets)
-
-    #save the id of the oldest tweet less one
-    oldest = alltweets[-1].id - 1
-
-    #keep grabbing tweets until the api limit is reached or all tweets are read
-    while (len(alltweets) <= 3200 and len(new_tweets) > 0):
-      print("getting tweets before {}".format(oldest))
-
-      #all subsiquent requests use the max_id param to prevent duplicates
-      new_tweets = api.user_timeline(screen_name = screen_name,tweet_mode = 'extended', count=200,max_id=oldest)
+    for screen_name in screen_names: 
+      #make initial request for most recent tweets with extended mode enabled to get full tweets
+      new_tweets = api.user_timeline(screen_name = screen_name, tweet_mode = 'extended', count=200)
 
       #save most recent tweets
       alltweets.extend(new_tweets)
 
-      #update the id of the oldest tweet less one
+      #save the id of the oldest tweet less one
       oldest = alltweets[-1].id - 1
 
-      print("...{} tweets downloaded so far".format(len(alltweets)))
+      #keep grabbing tweets until the api limit is reached or all tweets are read
+      while (len(alltweets) <= 3200 and len(new_tweets) > 0):
+        print("getting tweets before {}".format(oldest))
 
-      #removes retweets
-    for tweet in alltweets:
-      if 'RT' in tweet.full_text:
-        continue
-      else:
-        noRT.append(
-          [
-            tweet.id_str,
-            tweet.created_at,
-            re.sub('\\n+', '', tweet.full_text),
-            tweet.retweet_count,
-            tweet.favorite_count
-          ]
-        )
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,tweet_mode = 'extended', count=200,max_id=oldest)
+
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+
+        print("...{} tweets downloaded so far".format(len(alltweets)))
+
+        #removes retweets
+      for tweet in alltweets:
+        if 'RT' in tweet.full_text:
+          continue
+        else:
+          noRT.append(
+            [
+              tweet.id_str,
+              tweet.created_at,
+              tweet.user.screen_name,
+              re.sub('\\n+', '', tweet.full_text),
+              tweet.retweet_count,
+              tweet.favorite_count
+            ]
+          )
 
     #write to csv
-    file_name = './store_files/{}_tweets_{}.csv'.format(screen_name, timestamp)
+    file_name = './store_files/tweets_{}.csv'.format(timestamp)
     with open(file_name, 'w+') as f:
       writer = csv.writer(f, delimiter='\t')
       writer.writerows(noRT)
--- sql_files/create_twitter_data.sql
+++ sql_files/create_twitter_data.sql
@@ -1,3 +1,3 @@
-CREATE TABLE IF NOT EXISTS twitter_data(id varchar(20), created timestamp, content text, favorite_count int, retweet_count int);
+CREATE TABLE IF NOT EXISTS twitter_data(id varchar(20), created timestamp, screen_name varchar(20), content text, favorite_count int, retweet_count int);
 
 
