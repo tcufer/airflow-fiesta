bcf039e24bc68f2473abe201857edd748accfcad
tcufer <zamot101@gmail.com>
2020-11-16 23:53:41 +0100

    Query fix.

--- sql_files/select_tweets_by_popularity.sql
+++ sql_files/select_tweets_by_popularity.sql
@@ -1,3 +1,3 @@
-SELECT screen_name, content, favorite_count, retweet_count
+SELECT screen_name, content, favorite_count, retweet_count, created
 FROM twitter_data
 ORDER BY favorite_count DESC, retweet_count DESC
