14bacefdcb94f0189a7b1d137fa2edf600ecf969
tcufer <zamot101@gmail.com>
2020-11-17 23:53:20 +0100

    Group tweets by year.

--- sql_files/select_tweets_by_popularity.sql
+++ sql_files/select_tweets_by_popularity.sql
@@ -1,3 +1,9 @@
 SELECT screen_name, content, favorite_count, retweet_count, created
 FROM twitter_data
 ORDER BY favorite_count DESC, retweet_count DESC
+
+
+
+SELECT extract(year from created) as year, screen_name, content
+FROM twitter_data
+GROUP BY 1
