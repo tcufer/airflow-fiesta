dfe26b5cabb4873a055f9f4ec0f588da68271067
tcufer <zamot101@gmail.com>
2020-11-21 23:48:04 +0100

    More tests.

--- tests/dags/retrieve_tweets_tests.py
+++ tests/dags/retrieve_tweets_tests.py
@@ -5,9 +5,17 @@ class TestRetrieveTweets(unittest.TestCase):
 
     def setUp(self):
         self.dagbag = DagBag()
+        self.dag_id = 'retrieve_tweets'
+        self.tasks = ['twitter_feed', 'csv_to_postgres']
 
     def test_task_count(self):
         """Checks task count of dag"""
-        dag_id = 'retrieve_tweets'
-        dag = self.dagbag.get_dag(dag_id)
+        dag = self.dagbag.dags[self.dag_id]
         self.assertEqual(len(dag.tasks), 2)
+
+    def test_contain_tasks(self):
+        """Check task contains in retrieve_tweets dag"""
+        dag = self.dagbag.dags[self.dag_id]
+        tasks = dag.tasks
+        task_ids = list(map(lambda task: task.task_id, tasks))
+        self.assertListEqual(sorted(task_ids), sorted(self.tasks))
