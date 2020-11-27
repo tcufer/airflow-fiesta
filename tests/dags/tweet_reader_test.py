e2238d1979d1a719a42ef45d535e1ff22ae87099
tcufer <zamot101@gmail.com>
2020-11-27 23:46:40 +0100

    Nope.

--- tests/dags/tweet_reader_test.py
+++ tests/dags/tweet_reader_test.py
@@ -6,8 +6,8 @@ from dags import tweet_reader
 
 class TweetReaderTest(unittest.TestCase)
 
-    def get_all_tweets(self)
-        mock_get_patcher = patch('dags.requests.get')
+    # def get_all_tweets(self)
+    #     mock_get_patcher = patch('dags.requests.get')
 
-if __name__ == "__main__":
-    unittest.main()
+# if __name__ == "__main__":
+#     unittest.main()
