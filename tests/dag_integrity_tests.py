361889fe86b57613d40cbf467840c8d1563de252
tcufer <zamot101@gmail.com>
2020-12-02 23:50:21 +0100

    Added simple integrity tests.

---
+++ tests/dag_integrity_tests.py
@@ -0,0 +1,27 @@
+import unittest
+from airflow.models import DagBag
+
+
+class TestDagIntegrity(unittest.TestCase):
+    LOAD_SECOND_THRESHOLD = 1
+
+    def setUp(self):
+        self.dagbag = DagBag()
+
+    def test_import_dags(self):
+        self.assertFalse(
+            len(self.dagbag.import_errors),
+            'DAG import failures. Errors: {}'.format(
+                self.dagbag.import_errors
+            )
+        )
+
+    def test_import_time(self):
+        stats = self.dagbag.dagbag_stats
+        slow_dags = list(filter(lambda d: d.duration > self.LOAD_SECOND_THRESHOLD, stats))
+        res = ', '.join(map(lambda d: d.file[1:], slow_dags))
+
+        self.assertEqual(0, len(slow_dags),
+                          'The following files take more than {threshold}s to load: {res}'.format(
+                              threshold=self.LOAD_SECOND_THRESHOLD, res=res)
+                          )
