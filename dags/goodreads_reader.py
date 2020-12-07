5b0bc0a5dd541aa528005711fe31ba39dd1e27fb
tcufer <zamot101@gmail.com>
2020-12-07 23:25:42 +0100

    Read goodread quotes.

---
+++ dags/goodreads_reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import csv
+import pdb
+import yaml
+import re
+from datetime import datetime
+
+class GoodreadsReader():
+
+    def get_qutoes(self):
+        #@TODO
+
+
+if __name__ == '__main__':
+    GoodreadsReader().get_quotes()
