4cd6a8320b3289a565da5389bd7e4881afb4af74
tcufer <zamot101@gmail.com>
2020-12-09 23:46:43 +0100

    Testing source.

--- dags/goodreads_reader.py
+++ dags/goodreads_reader.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
-import csv
-import pdb
-import yaml
-import re
-from datetime import datetime
+# import csv
+# import pdb
+# import yaml
+# import re
+# from datetime import datetime
+from bs4 import BeautifulSoup 
+import requests
+import pprint
+# class GoodreadsReader():
 
-class GoodreadsReader():
-
-    def get_qutoes(self):
-        #@TODO
+#     def get_qutoes(self):
+#         #@TODO
 
 
 if __name__ == '__main__':
-    GoodreadsReader().get_quotes()
-
+    # GoodreadsReader().get_quotes()
+    results = []
+    url = "https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q=stoicism&commit=Search"   
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    for quote in soup.find_all("div", class_= "quote"):
+        squote = {}
+        squote['text']= quote.find("div", {"class": "quoteText"}).text.replace('\n','').strip()
+        squote['author'] = quote.find("span", {"class": "authorOrTitle"}).text.replace('\n','').strip()  
+        leftAlignedImage = quote.find("a", {"class": "leftAlignedImage"})
+        squote['image'] = leftAlignedImage.img['src'] if leftAlignedImage else None
+        quoteFooter = quote.find("div", {"class": "quoteFooter"})
+        squote['tags'] = [tag.text.strip() for tag in quoteFooter.find_all("a") if tag and "likes" not in tag.text]
+        results.append(squote)
+    
+    pprint.pprint(results)
+    
 # https://github.com/CW4RR10R/Quotes-API
