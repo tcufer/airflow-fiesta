afff23ecab55397c189c3596a5efaafec5cee866
tcufer <zamot101@gmail.com>
2021-01-02 23:37:02 +0100

    Prepare tests for goodreads_reader.

---
+++ tests/dags/goodreads_reader_tests.py
@@ -0,0 +1,11 @@
+# import unittest
+
+# class TestGoodreadsReader(unittest.TestCase):
+
+# def setUp(self):
+
+    # def test_compose_url(self):
+
+    # def test_generate_quote_id(self):
+
+    # def test_get_quotes(self):
